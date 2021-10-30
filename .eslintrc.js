fdbee8e12a6e64b0549cd11357bc932006ea6067
Akshay Jumbade <akkyjumbade@gmail.com>
2021-10-30 20:23:50 +0530

    added template

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-   root: true,
-   extends: '@react-native-community',
-   rules: {
-      'prettier/prettier': 0,
-      'comma-dangle': 0,
-      'react/self-closing-comp': 0,
-   }
-};
