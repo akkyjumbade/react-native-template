19e9754895f45f6c884d142acbeac6e343c16813
Akshay Jumbade <akkyjumbade@gmail.com>
2021-10-30 18:48:34 +0530

    framework added

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+   root: true,
+   extends: '@react-native-community',
+   rules: {
+      'prettier/prettier': 0,
+      'comma-dangle': 0,
+      'react/self-closing-comp': 0,
+   }
+};
---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+   SafeAreaView,
+} from 'react-native';
+
+
+const App = () => {
+   return (
+      <SafeAreaView >
+
+      </SafeAreaView>
+   );
+};
+
+export default App;
