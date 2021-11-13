2a2c5132a2c56261181bbab97959981c55d4e467
Akshay Jumbade <akkyjumbade@gmail.com>
2021-11-13 17:22:40 +0530

    added statusbar style support

--- RNProject/packages/rn-kit/src/provider/ThemeContextProvider.js
+++ RNProject/packages/rn-kit/src/provider/ThemeContextProvider.js
@@ -1,10 +1,18 @@
-import React, { Fragment, memo } from 'react'
+import React, { memo } from 'react'
 import { ThemeProvider } from 'styled-components'
 import theme from '../themes/theme'
+import {StatusBar} from "react-native";
 
 const ThemeContextProvider = ({ children, theme: providedTheme }) => {
    return (
       <React.StrictMode>
+         <StatusBar
+            animated={true}
+            backgroundColor={providedTheme.colors?.primary ?? 'black'}
+            hidden={false}
+            barStyle={'dark-content'}
+            showHideTransition={'fade'}
+         />
          <ThemeProvider theme={providedTheme}>
             {children}
          </ThemeProvider>
